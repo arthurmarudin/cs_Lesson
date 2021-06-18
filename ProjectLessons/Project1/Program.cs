487ba37e90ce2bf862bdcbe54d3a7b8c1b1874e2
MarudinArthur <marudin.dev@gmail.com>
2021-06-18 15:20:21 +0300

    fix

--- ProjectLessons/Project1/Program.cs
+++ ProjectLessons/Project1/Program.cs
@@ -1,55 +1,56 @@
-﻿	using System;
+﻿		using System;
 
-	namespace Project1
-	{
-		class Program
+		namespace Project1
 		{
-			static void Main(string[] args)
+			class Program
 			{
-
-				Console.WriteLine("Main menu:\n");
-				Console.WriteLine("1. Play game\n2. Settings\n3. Exit");
-				Console.WriteLine("\n..please enter the number of the menu item..\n");
-				int playerChoice = Convert.ToInt32(Console.ReadLine());
-
-				switch (playerChoice)
+				static void Main(string[] args)
 				{
-					case 1:
-						Console.WriteLine("\nYou choice menu item - Play Game.");
-						break;
-					case 2:
-						Console.WriteLine("\nYou choice menu item - Settings.");
-						break;
-					case 3:
-						Console.WriteLine("\nYou choice menu item - Exit.");
-						break;
-					default:
-						Console.WriteLine("\nError. Please enter a valid value.");
-						break;
-				}
 
-				if (playerChoice == 1)
-				{
-					Console.WriteLine("\nStart Game.");
-					//и сюда вставляем методы/классы
-				}
-				else if (playerChoice == 2)
-				{
-					Console.WriteLine("\nGame settings:");
-					//и сюда вставляем методы/классы
-				}
-				else if (playerChoice == 3)
-				{
-					Console.WriteLine("\nGoodbye.");
-				}
-			}
-		/*
-			public static void Settings()
-			{
-				Console.WriteLine("\n1. Sound.\n2. Music.\n3.Haptic.");
-				int playerSettingChoice = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("Main menu:\n");
+					Console.WriteLine("1. Play game\n2. Settings\n3. Exit");
+					Console.WriteLine("\n..please enter the number of the menu item..\n");
+					int playerChoice = Convert.ToInt32(Console.ReadLine());
 
-			}
+					switch (playerChoice)
+					{
+						case 1:
+							Console.WriteLine("\nYou choice menu item - Play Game.");
+							break;
+						case 2:
+							Console.WriteLine("\nYou choice menu item - Settings.");
+							break;
+						case 3:
+							Console.WriteLine("\nYou choice menu item - Exit.");
+							break;
+						default:
+							Console.WriteLine("\nError. Please enter a valid value.");
+							break;
+					}
+
+					if (playerChoice == 1)
+					{
+						Console.WriteLine("\nStart Game.");
+						//и сюда вставляем методы/классы
+					}
+					else if (playerChoice == 2)
+					{
+						Console.WriteLine("\nGame settings:");
+						//и сюда вставляем методы/классы
+					}
+					else if (playerChoice == 3)
+					{
+						Console.WriteLine("\nGoodbye.");
+					}
+				}
+			/*
+				public static void Settings()
+				{
+					Console.WriteLine("\n1. Sound.\n2. Music.\n3.Haptic.");
+					int playerSettingChoice = Convert.ToInt32(Console.ReadLine());
+
+				}
+			*/
 		}
 	}
-		*/
+		
