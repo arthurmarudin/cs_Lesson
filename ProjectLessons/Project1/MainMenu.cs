fed19d68a3f9299fcb83d5808a3d8c7c3c849456
MarudinArthur <marudin.dev@gmail.com>
2021-06-21 17:10:55 +0300

    add new logic for main

---
+++ ProjectLessons/Project1/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Game;
+
+
+namespace Main
+{
+	public class MainMenu
+	{
+		public void menuItem(int x)
+		{
+            switch (x)
+			{
+				case 1:
+				Console.WriteLine("\nYou choice menu item - Play Game.");
+                break;
+				case 2:
+				Console.WriteLine("\nYou choice menu item - Play Game.");
+				break;
+				case 3:
+				Console.WriteLine("\nYou choice menu item - Settings.");
+				break;
+				case 4:
+				Console.WriteLine("\nYou choice menu item - Exit.");
+				break;
+				default:
+				Console.WriteLine("\nError. Please enter a valid value.");
+				break;
+
+			}
+					if (x == 1)
+					{
+						Console.WriteLine("\nStart Game.");
+						DiceRoller game = new DiceRoller();
+						game.Roller();
+					}
+					else if (x == 2)
+					{
+						Console.WriteLine("\nGame rules:");
+						//и сюда вставляем методы/классы
+					}
+					else if (x == 3)
+					{
+						Console.WriteLine("\nGame settings:");
+						//и сюда вставляем методы/классы
+					}
+					else if (x == 4)
+					{
+						Console.WriteLine("\nGoodbye.");
+					}
+		}
+	}
+}
--- ProjectLessons/Project1/Program.cs
+++ ProjectLessons/Project1/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using Game;
+using Main;
 
 namespace Project1
 {
-    class Program
+	class Program
 	{
 		static void Main(string[] args)
 		{
@@ -13,54 +14,9 @@ namespace Project1
 			Console.WriteLine("\n..please enter the number of the menu item..\n");
 			int playerChoice = Convert.ToInt32(Console.ReadLine());
 
-			switch (playerChoice)
-			{
-				case 1:
-					Console.WriteLine("\nYou choice menu item - Play Game.");
-					break;
-				case 2:
-					Console.WriteLine("\nYou choice menu item - Rules.");
-					break;
-				case 3:
-					Console.WriteLine("\nYou choice menu item - Settings.");
-					break;
-				case 4:
-					Console.WriteLine("\nYou choice menu item - Exit.");
-					break;
-				default:
-					Console.WriteLine("\nError. Please enter a valid value.");
-					break;
-			}
-
-			if (playerChoice == 1)
-			{
-				Console.WriteLine("\nStart Game.");
-				DiceRoller game = new DiceRoller();
-				game.Roller();
-			}
-			else if (playerChoice == 2)
-			{
-				Console.WriteLine("\nGame rules:");
-				//и сюда вставляем методы/классы
-			}
-			else if (playerChoice == 3)
-			{
-				Console.WriteLine("\nGame settings:");
-				//и сюда вставляем методы/классы
-			}
-			else if (playerChoice == 4)
-			{
-				Console.WriteLine("\nGoodbye.");
-			}
+            MainMenu main = new MainMenu();
+            main.menuItem(playerChoice);
 		}
-	/*
-		public static void Settings()
-		{
-			Console.WriteLine("\n1. Sound.\n2. Music.\n3.Haptic.");
-			int playerSettingChoice = Convert.ToInt32(Console.ReadLine());
 
-		}
-	*/
+	}
 }
-			}
-		
